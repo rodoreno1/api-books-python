62235665233ee309dac23241b3e4b618cac4b73a
Rodolfo Bautista <Rodolfo.Bautista@solera.com>
2024-07-25 15:07:06 -0600

    Update demo_Relationship to run the project

--- examples/demo_relationship.py
+++ examples/demo_relationship.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python3
-"""
-  This demo application demonstrates the functionality of the safrs documented REST API
-  When safrs is installed, you can run this app:
-  $ python3 demo_relationship.py [Listener-IP]
-
-  This will run the example on http://Listener-Ip:5000
-
-  - An sqlite database is created and populated
-  - A jsonapi rest API is created
-  - Swagger documentation is generated
-
-"""
 import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -24,25 +11,21 @@ class User(SAFRSBase, db.Model):
     """
     description: User description
     """
-
     __tablename__ = "Users"
     id = db.Column(db.String, primary_key=True)
     name = db.Column(db.String, default="")
     email = db.Column(db.String, default="")
     books = db.relationship("Book", back_populates="user", lazy="dynamic")
 
-
 class Book(SAFRSBase, db.Model):
     """
     description: Book description
     """
-
     __tablename__ = "Books"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, default="")
     user_id = db.Column(db.String, db.ForeignKey("Users.id"))
     user = db.relationship("User", back_populates="books")
-
 
 # Create the api endpoints
 def create_api(app, host="localhost", port=5000, api_prefix=""):
@@ -51,15 +34,14 @@ def create_api(app, host="localhost", port=5000, api_prefix=""):
     api.expose_object(Book)
     print(f"Created API: http://{host}:{port}/{api_prefix}")
 
-
-def create_app(config_filename=None, host="localhost"):
+def create_app(config_filename=None, host="localhost", port=5000):
     app = Flask("demo_app")
     app.config.update(SQLALCHEMY_DATABASE_URI=f"sqlite://")
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
-        create_api(app, host)
+        create_api(app, host, port)
         # Populate the db with users and a books and add the book to the user.books relationship
         for i in range(200):
             user = User(name=f"user{i}", email=f"email{i}@email.com")
@@ -68,10 +50,8 @@ def create_app(config_filename=None, host="localhost"):
             
     return app
 
-
-# Address where the api will be hosted, change this if you're not running the app on localhost!
-host = sys.argv[1] if sys.argv[1:] else "127.0.0.1"
-app = create_app(host=host)
-
 if __name__ == "__main__":
-    app.run(host=host)
+    host = sys.argv[1] if len(sys.argv) > 1 else "0.0.0.0"
+    port = int(sys.argv[2]) if len(sys.argv) > 2 else 5000
+    app = create_app(host=host, port=port)
+    app.run(host=host, port=port)
